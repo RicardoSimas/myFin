0e8e2c0d9f0c714c428fd566e4267466aa9d6d40
Ricardo Simas <rsimas@unigranrio.br>
2021-07-05 21:20:36 -0300

    Add testes de integracao para insert e search usuario na base

--- src/test/java/com/rsimas/myfin/repositories/UsuarioRepositoryTest.java
+++ src/test/java/com/rsimas/myfin/repositories/UsuarioRepositoryTest.java
@@ -1,48 +1,94 @@
 package com.rsimas.myfin.repositories;
 
+import java.util.Optional;
+
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.rsimas.myfin.domain.Usuario;
 
-@SpringBootTest
+@DataJpaTest
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = Replace.NONE)
 public class UsuarioRepositoryTest {
-	
+
 	@Autowired
 	private UsuarioRepository repo;
-		
-	@org.junit.jupiter.api.Test
+
+	@Autowired
+	TestEntityManager entityManager;
+
+	@Test
 	public void deveVerificarExistenciaDeEmail() {
-		//Cenario
-		Usuario user = Usuario.builder()
-				.nome("Usuario")
-				.email("Usuario@email.com")
-				.build();
-		
-		repo.save(user);
-		
-		//Execução
+		// Cenario
+		Usuario user = criarUsuario();
+
+		entityManager.persist(user);
+
+		// Execução
 		boolean result = repo.existsByEmail("Usuario@email.com");
-		
-		//Verifição
+
+		// Verifição
 		Assertions.assertThat(result).isTrue();
 	}
-	
-	@org.junit.jupiter.api.Test
+
+	@Test
 	public void deveRetornarFalsoQuandoNaoExistirUsuarioComOEmail() {
-		//Cenario
-		repo.deleteAll();
-		
-		//Execução
+		// Cenario
+		// DataJpaTest após o término da transação, efetua ROLLBACK.
+
+		// Execução
 		boolean result = repo.existsByEmail("Usuario@email.com");
-		
-		//Verifição
+
+		// Verifição
 		Assertions.assertThat(result).isFalse();
 	}
+
+	@Test
+	public void devePersistirUmUsuario() {
+		// cenário
+		Usuario usuario = criarUsuario();
+
+		// Execução
+		Usuario usuarioSalvo = repo.save(usuario);
+
+		// Verificação
+		Assertions.assertThat(usuarioSalvo.getId()).isNotNull();
+	}
+
+	@Test
+	public void deveBuscarUmUsuarioPorEmail() {
+		// cenario
+		Usuario usuario = criarUsuario();
+		entityManager.persist(usuario);
+
+		// execução
+		Optional<Usuario> user = repo.findByEmail(usuario.getEmail());
+
+		// verificação
+		Assertions.assertThat(user.isPresent()).isTrue();
+	}
+
+	@Test
+	public void deveRetornarVazioAoUsuarioPorEmailSeNaoHouverNaBase() {
+		// execução
+		Optional<Usuario> user = repo.findByEmail("Usuario@email.com");
+
+		// verificação
+		Assertions.assertThat(!user.isPresent()).isTrue();
+	}
+
+	public static Usuario criarUsuario() {
+		return Usuario.builder().nome("Usuario").email("Usuario@email.com").senha("senha").build();
+	}
+
 }
--- src/test/java/com/rsimas/myfin/services/UsuarioServiceTest.java
+++ src/test/java/com/rsimas/myfin/services/UsuarioServiceTest.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -12,9 +14,10 @@ import com.rsimas.myfin.domain.Usuario;
 import com.rsimas.myfin.exceptions.RegraNegocioException;
 import com.rsimas.myfin.repositories.UsuarioRepository;
 
-@SpringBootTest
+@DataJpaTest
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = Replace.NONE)
 public class UsuarioServiceTest {
 	
 	@Autowired
