56314a7ed9cbf1ddec100f065695782e0fcde9f0
Ricardo Simas <rsimas@unigranrio.br>
2021-07-09 15:56:34 -0300

    Buscando lancamentos filtrados com GET.

--- src/main/java/com/rsimas/myfin/resources/LancamentoResource.java
+++ src/main/java/com/rsimas/myfin/resources/LancamentoResource.java
@@ -1,26 +1,57 @@
 package com.rsimas.myfin.resources;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rsimas.myfin.domain.Lancamento;
+import com.rsimas.myfin.domain.Usuario;
 import com.rsimas.myfin.dto.LancamentoDTO;
 import com.rsimas.myfin.exceptions.RegraNegocioException;
 import com.rsimas.myfin.services.LancamentoService;
+import com.rsimas.myfin.services.UsuarioService;
 
 @RestController
 @RequestMapping(value = "/api/lancamentos")
 public class LancamentoResource {
 
 	private LancamentoService service;
+	private UsuarioService usuarioService;
 
 	public LancamentoResource(LancamentoService service) {
 		this.service = service;
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity buscar(
+			@RequestParam(value = "descricao", required = false) String descricao,
+			@RequestParam(value = "mes", required = false) Integer mes,
+			@RequestParam(value = "ano", required = false) Integer ano,
+			@RequestParam("usuario") Long idUsuario) {
+		
+		Lancamento lancamentoFiltro = new Lancamento();
+		lancamentoFiltro.setDescricao(descricao);
+		lancamentoFiltro.setMes(mes);
+		lancamentoFiltro.setAno(ano);
+		
+		Optional<Usuario> usuario = usuarioService.buscarPorId(idUsuario);
+		if(!usuario.isPresent()) {
+			return ResponseEntity.badRequest().body("Impossível realizar consulta. Usuario não encontrado na base!");
+		}else {
+			lancamentoFiltro.setUsuario(usuario.get());
+		}
+		
+		List<Lancamento> lancamentos = service.buscar(lancamentoFiltro);
+		
+		return ResponseEntity.ok(lancamentos);
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
